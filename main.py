89e665bae072c7e9dc2d11d142fbeb48885fb171
Simon <astrid.lindgren@gmail.com>
2026-06-04 17:44:47 +0200

    tada

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 import sys
+import install
 
 print("Rotarium\n")
-helptext = """
-usage:
+helptext = """usage:
     python main.py install <package>
     python main.py remove <package>
-    python main.py help
-"""
+    python main.py help"""
+
 if len(sys.argv) < 2:
     print(helptext)
     sys.exit()
+
 arg = sys.argv[1]
-
-
 if arg == "install":
-    print("stub")
+    if len(sys.argv) < 3:
+        print("you didnt specify a package")
+    else:
+        print("so uhh i forgot to implement this part, be right back!")
 elif arg == "remove":
     print("stub")
 elif arg == "help":
